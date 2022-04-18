72c6f756aa7a8361ba405dc6359adb25f8878b0a
nasu105 <fuyuyasai105@gmail.com>
2022-04-18 21:29:52 +0900

    uprode

---
+++ js/hira.js
@@ -0,0 +1,28 @@
+// document.getElementById("method").style.display = "none";
+
+// function clickBtn1() {
+//     const choose_method = document.getElementById("choose_method");
+//     choose_method.style.display = "block";
+// }
+
+// function clickBtn2() {
+//     const choose_method = document.getElementById("choose_method");
+//     choose_method.style.display = "block";
+// }
+
+/*
+function clickBtn1() {
+    const method = document.getElementById("#home");
+    method.style.display = "block";
+}
+*/
+// $(function(){
+//     $("#high_wood").on("click", function () {
+//         const randomnumber1 = Math.floor(Math.random() * 5);
+//         if (randomnumber1 < 4) {
+//             console.log('成功');
+//         } else if (randomnumber1 === 4) {
+//             console.log('失敗');
+//         } else { console.log('それ以外'); }
+//     });
+// )};
---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    $("#hira_btn").on("click", function () {
+
+        window.location.href = 'hira.html'
+
+    });
+});
+
+
+$(function () {
+
+    $("#butyo_btn").on("click", function () {
+
+        window.location.href = 'butyo.html'
+
+    });
+});
+
+
+$(function () {
+
+    $("#syatyo_btn").on("click", function () {
+
+        window.location.href = 'syatyo.html'
+
+    });
+});
